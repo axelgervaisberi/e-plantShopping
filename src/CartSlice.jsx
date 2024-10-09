5f433b749ce044fbeca732a4b2d4e3b04edaefe2
Gervais BERI <beriaxel@gmail.com>
2024-10-09 01:23:05 +0000

    Displyed products from plantsArray

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,20 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      // Add the new item to the cart items array
+      const existingItem = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        // If the item already exists, update its quantity
+        existingItem.quantity += action.payload.quantity;
+      } else {
+        // If the item doesn't exist, add it to the cart
+        state.items.push(action.payload);
+      }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
+    removeItem: (state, action) => {},
+    updateQuantity: (state, action) => {},
   },
 });
 
